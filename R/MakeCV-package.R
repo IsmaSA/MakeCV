b8383f68665fce93b2a1b86d581acaaafdfad6e6
Ismael Soto <isma-sa@hotmail.com>
2024-09-18 21:18:54 +0200

    Create MakeCV-package.R

---
+++ R/MakeCV-package.R
@@ -0,0 +1,13 @@
+#' MakeCV package
+#'
+#' This package contains tools and functions to help prepare a  CV
+#' (with citations, altmetrics, etc), plus CV analysis tools (e.g. citation or coauthorship networks).
+#' It is particularly aimed for CVs maintained in Rmarkdown format.
+#'
+#' @name MakeCV-package
+#' @aliases MakeCV
+#' @docType package
+#' @author Ismael Soto
+#' @keywords package
+#'
+NULL
